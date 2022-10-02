afa7f32f909cb9fe5c58362cc10aded982ba2cc8
Shashwat <shashwatvirat5127@gmail.com>
2022-10-02 23:23:43 +0530

    button added with restructed grid based on use input

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 const containerDiv = document.querySelector("#container");
 
-function makeRows(rows,coloumns) {
+function makeGrid(rows,coloumns) {
+
+    while (document.querySelector("button") !== null) {
+        document.querySelector("button").remove();
+    }
+
     containerDiv.style.setProperty("--grid-rows",rows);
     containerDiv.style.setProperty("--grid-coloumns",coloumns);
+    containerDiv.style.width = "auto";
+    containerDiv.style.overflow = "hidden";
 for (i=0;i < (rows * coloumns); i++) {
     let cell =document.createElement("div");
-    
-    containerDiv.appendChild(cell).className="grid-item";
-};
-};
+    cell.style.minHeight="0";
+    cell.style.minWidth="0";
+   
 
-makeRows(16,16);
+    containerDiv.appendChild(cell).className="grid-item";
+    cell.addEventListener("mouseover",(event) =>    {
+        event.target.style.backgroundColor="purple";
+    })
+}
+createbutton();
+}
+
+
+function createbutton() {
+    const buttonDiv = document.querySelector("#buttonDiv");
+    const resetbuttom = document.createElement("button");
+    resetbuttom.innerText="Reset Grid";
+    resetbuttom.style.margin = "20px";
+    resetbuttom.style.padding = "50px";
+buttonDiv.appendChild(resetbuttom);
+
+
+resetbuttom.addEventListener("click", () => {
+document.querySelectorAll(".grid-item").forEach(e => e.remove());
+let usergridinput = prompt("enter a number below 100");
+if (usergridinput < 0 || usergridinput > 100) {
+    alert("pls enter a number between 1 and 100 only");
+    return;
+}
+rows = usergridinput;
+coloumns =usergridinput;
+makeGrid(rows,coloumns)
+})
+}
+
+
+makeGrid(16,16)
